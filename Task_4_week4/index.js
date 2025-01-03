8cd340ac21ba3b0a6615dde7b0e6435b9c6d4116
Elsabeth721 <elsabethzeleke1000@gmail.com>
2025-01-03 11:01:03 +0300

    basic CRUD API in Nodejs

---
+++ Task_4_week4/index.js
@@ -0,0 +1,81 @@
+const http =require('http');
+const port =process.env.PORT || 3000;
+const {parse} =require('url');
+
+let users=[];
+let nextId= 1;
+
+const server =http.createServer((req, res)=>{
+    const parsedUrl =parse(req.url, true);
+    const method=req.method;
+
+    res.setHeader('Content-Type', 'application/json');
+
+    //GET /users: Return all users
+    if(method == '/GET' && parsedUrl.pathname === '/users'){
+        res.writeHead(200);
+        res.end(JSON.stringify(users));
+    }
+    else if(method == 'POST' && parsedUrl.pathname === '/users'){
+        let body ='';
+
+        req.on('data', chunk =>{
+            //each chunk is a buffer needs to be converted to string
+            body+= chunk.toString();
+        })
+        req.on('end', ()=>{
+            const newUSer =JSON.parse(body);
+            newUSer.id=nextId++;
+            users.push(newUser);
+            res.writeHead(201);//201 --new user has been successfully created
+            res.end(JSON.stringify(newUser));
+        })
+    }
+    else if(method === '/PUT' && parsedUrl.pathname.startsWith === '/users/'){ // targeting users based on their id
+        const id = parseInt(parsedUrl.pathname.split('/')[2], 10);  //extract user ID from the URL
+        let body ='';
+        req.on('data', chunk=>{
+            body += chunk.toString();
+        })
+        req.on('end',()=>{
+            const updatedData =JSON.parse(body);
+            const index =users.findIndex(user=>user.id === id);
+            if(index!== -1){
+                users[index]={...users[index], ...updatedData};
+                res.writeHead(200); // 200 --ok update was successful
+                res.end(JSON.stringify(users[index]));
+            }
+            else{
+                res.writeHead(404); //404  not found status code 
+                res.end(JSON.stringify({message: 'user not found'}));
+            }
+        })
+
+    }
+    else if(method === '/DELETE' && parsedUrl.pathname.startsWith === '/users/'){ // targeting users based on their id
+        const id = parseInt(parsedUrl.pathname.split('/')[2], 10);  //extract user ID from the URL
+            const index =users.findIndex(user=>user.id === id);
+            if(index!== -1){
+                users.splice(index, 1);
+                res.writeHead(200); // 200 --ok update was successful
+                res.end(JSON.stringify(users[index]));
+            }
+            else{
+                res.writeHead(404); //404  not found status code 
+                res.end(JSON.stringify({message: 'user not found'}));
+            }
+    }
+    else{
+        res.writeHead(404);
+        res.end(JSON.stringify({message: 'Not found'}));
+    }
+
+});
+
+server.listen(port, ()=>{
+    console.log("App is running on port", port);
+});
+
+    
+
+
