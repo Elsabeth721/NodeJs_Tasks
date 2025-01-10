33e63d7d0c8d02ba21a51bc43ba4799fc03e867f
Elsabeth721 <elsabethzeleke1000@gmail.com>
2025-01-10 22:06:15 +0300

    CRUD API using express

--- CRUD_API_Express/index.js
+++ CRUD_API_Express/index.js
@@ -1,9 +1,21 @@
 const express =require("express");
 const bodyParser =require("body-parser");
 const app= express();
+app.use(bodyParser.json());
 const port =process.env.PORT ||3000;
 
-app.use(express.json());
+let users =[];
+let nextId = 1;
+
+app.get('/users', (req, res)=>{
+    res.status(200).send(users);
+});
+
+app.post('/users', (req,res)=>{
+    const newUser ={id: users.length+1, ...req.body};
+    users.push(newUser);
+    res.status(201).send(newUser);
+})
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
 });
